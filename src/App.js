a4fdcb2c1e1da697c43e258ae2e0526278b587f6
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-03-15 01:57:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [percentResult, setPercentResult] = useState(0);
+  const [number, setNumber] = useState("");
+  const [outOf, setOutOf] = useState("");
+
+  const calculatePercent = () => {
+    const result = (number / outOf) * 100;
+    setPercentResult(result);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>Percent calculator</h2>
+          <div>
+            <input
+              type="number"
+              data-testid="number"
+              placeholder="Number"
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              data-testid="outof"
+              placeholder="Out of"
+              onChange={(e) => setOutOf(e.target.value)}
+            />
+          </div>
+          <div>
+            <button onClick={() => calculatePercent()} data-testid="button">
+              Calculate Percentage
+            </button>
+          </div>
+          <p>
+            Result - <span data-testid="result">{percentResult}</span>%
+          </p>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Percentage calculator", () => {
+  it("render a percentage calculator", () => {
+    render(<App />);
+    // check if components are rendered
+    expect(screen.getByTestId("number")).toBeInTheDocument();
+    expect(screen.getByTestId("outof")).toBeInTheDocument();
+    expect(screen.getByTestId("result")).toBeInTheDocument();
+  });
+  it("should calculate percentage correctly", () => {
+    render(<App />);
+    // set input values
+    fireEvent.change(screen.getByTestId("number"), {
+      target: { value: 10 },
+    });
+    fireEvent.change(screen.getByTestId("outof"), {
+      target: { value: 20 },
+    });
+    // click the button
+    fireEvent.click(screen.getByTestId("button"));
+    // check if the result is correct
+    expect(screen.getByTestId("result").textContent).toBe("50");
+  });
 });
